3e1d5c296ee28d0649418d89407b4d7212f5ae3e
suzb <suzb@bwoil.com>
2023-07-03 09:21:53 +0800

    Initial commit

---
+++ app/src/main/java/com/show/gittest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.show.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Create by suzb on 2023/7/3
+ * GitTest
+ * com.show.gittest
+ */
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
